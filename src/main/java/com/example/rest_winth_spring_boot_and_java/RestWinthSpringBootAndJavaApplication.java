a407e31b0a61b2ade37caa331edd93174bab5a79
mateuspds <mateus.silva09082000@gmail.com>
2025-12-02 14:40:02 -0300

    estrutura inicial do projeto

---
+++ src/main/java/com/example/rest_winth_spring_boot_and_java/RestWinthSpringBootAndJavaApplication.java
@@ -0,0 +1,15 @@
+package com.example.rest_winth_spring_boot_and_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWinthSpringBootAndJavaApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWinthSpringBootAndJavaApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/example/rest_winth_spring_boot_and_java/RestWinthSpringBootAndJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rest_winth_spring_boot_and_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWinthSpringBootAndJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
