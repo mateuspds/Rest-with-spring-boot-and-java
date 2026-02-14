2362b85d2a81e431a2db77a8ebbf0e7eb3612d70
mateuspds <mateus.silva09082000@gmail.com>
2026-02-13 22:42:01 -0300

    adicionando o rest de math ao projeto

---
+++ src/main/java/com/example/rest_winth_spring_boot_and_java/controllers/MathController.java
@@ -0,0 +1,11 @@
+package com.example.rest_winth_spring_boot_and_java.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/math")
+public class MathController {
+
+
+}
